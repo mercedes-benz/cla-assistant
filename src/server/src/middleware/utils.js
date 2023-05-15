59725fdd4b2484d42e2484d7ebcf76b5f5ec0108
Aravind Ramakrishnan <aravind.ramakrishnan@mercedes-benz.com>
2023-05-15 14:03:14 +0530

    feat(): Update for admin feat

--- src/server/src/middleware/utils.js
+++ src/server/src/middleware/utils.js
@@ -4,12 +4,18 @@
 
 const githubService = require('../services/github')
 const log = require('../services/logger')
+const Joi = require('joi')
+const config = require('../config')
 
 class Utils {
     couldBeAdmin(username) {
         return config.server.github.admin_users.length === 0 || config.server.github.admin_users.indexOf(username) >= 0
     }
 
+    adminModeEnabled() {
+        return config.server.github.admin_users.length > 0
+    }
+    
     async checkRepoPushPermissionByName(repo, owner, repoId, token) {
         try {
             let res, repoData
