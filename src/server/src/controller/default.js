2821bdce2308cd992c908293ea582878e08b3426
Anisha <62882541+AnishaM7@users.noreply.github.com>
2023-09-11 15:43:00 +0530

    Update default.js
    testing

--- src/server/src/controller/default.js
+++ src/server/src/controller/default.js
@@ -91,13 +91,16 @@ router.all('/*', (req, res) => {
     if (req.path === '/robots.txt') {
         filePath = path.join(__dirname, '..', '..', '..', 'client', 'assets', 'robots.txt')
     }
+    else if (((req.user && req.user.scope && req.user.scope.indexOf('write:repo_hook') > -1) || req.path !== '/') && adminModeEnabled()) {
+        filePath = path.join(__dirname, '..', '..', '..', 'client', 'assets', 'templates', 'my-cla.html')
+    }
     else if ((req.user && req.user.scope && req.user.scope.indexOf('write:repo_hook') > -1) || req.path !== '/') {
         filePath = path.join(__dirname, '..', '..', '..', 'client', 'assets', 'home.html')
-    } else {
+    }
+    else {
         filePath = config.server.templates.login
     }
     res.setHeader('Last-Modified', (new Date()).toUTCString())
     res.status(200).sendFile(filePath)
 })
-
 module.exports = router
